56e1c94d0d60280d11060887cbd34ef681dd50bb
Lucas Coffey <lbc2k1385@gmail.com>
2021-05-08 17:33:44 -0400

    Added security

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-from profiles_api import serializers
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import serializers, models, permissions
 
 
 class HelloApiView(APIView):
@@ -84,3 +85,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Delete object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserprofileViewSet(viewsets.ModelViewSet):
+    """"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
+
+
